a6f0a570ce0388263c9a7167e99f62f12074c454
Doeun <doeun990802@gmail.com>
2024-01-02 23:41:28 +0900

    [fix] 친구 추가 기능 수정 - userId 추가

--- src/controller/friendController.js
+++ src/controller/friendController.js
@@ -3,9 +3,11 @@ const { response, errResponse } = require('../../config/response')
 const friendService = require('../service/friendService')
 
 exports.addNewFriend = async function (req, res) {
+    const userId = req.user_id
     const { nickname, relation, favor } = req.body
 
     const addNewFriendResult = await friendService.addFriend(
+        userId,
         nickname,
         relation,
         favor
