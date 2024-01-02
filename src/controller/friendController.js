8f53787a7cf1b5a71bfa722ce3c8dd3c44c857ab
Doeun <doeun990802@gmail.com>
2024-01-03 03:39:01 +0900

    [fix] 친구 추가 기능 - favor 삭제

--- src/controller/friendController.js
+++ src/controller/friendController.js
@@ -3,14 +3,13 @@ const { response, errResponse } = require('../../config/response')
 const friendService = require('../service/friendService')
 
 exports.addNewFriend = async function (req, res) {
-    const userId = req.user_id
-    const { nickname, relation, favor } = req.body
+    // const userId = req.user_id
+    const { nickname, relation } = req.body
 
     const addNewFriendResult = await friendService.addFriend(
         req.user_id,
         nickname,
-        relation,
-        favor
+        relation
     )
     
     return res.send(addNewFriendResult)
--- src/dao/friendDao.js
+++ src/dao/friendDao.js
@@ -1,5 +1,5 @@
 async function addFriend(connection, newFriendParams) {
-    const addFriendQuery = `INSERT INTO friend (user_id, nickname, relation, favor) values (?, ?, ?, ?);`;
+    const addFriendQuery = `INSERT INTO friend (user_id, nickname, relation) values (?, ?, ?);`;
     return await connection.query(addFriendQuery, newFriendParams);
 }
 
