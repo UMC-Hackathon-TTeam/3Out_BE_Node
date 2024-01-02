5179767f916acf58e505ee7f87e9f60dd134441c
Jayde1012 <ariha1982@naver.com>
2024-01-03 05:50:35 +0900

    [REFACTOR] 이것저것

--- src/controller/friendController.js
+++ src/controller/friendController.js
@@ -5,7 +5,7 @@ const friendPRO = require("../provider/friendProvider")
 
 exports.addNewFriend = async function (req, res) {
     // const userId = req.user_id
-    const { nickname, relation } = req.body
+    const { nickname, relation, image } = req.body
 
     const addNewFriendResult = await friendService.addFriend(
         req.user_id,
