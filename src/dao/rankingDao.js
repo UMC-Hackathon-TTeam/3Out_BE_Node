c1e72ffec1661272a6113d575f928f6473a08e66
Doeun <doeun990802@gmail.com>
2024-01-03 04:52:47 +0900

    [fix] 친구 랭킹 조회 - dao image 필드 추가

--- src/dao/rankingDao.js
+++ src/dao/rankingDao.js
@@ -3,7 +3,7 @@ async function getRankingsByStickerId(connection, userId, stickerId) {
 
     if (stickerId === 1) {
         getRankingsQuery = `
-            SELECT fs.friend_id, f.nickname, COUNT(*) AS sticker_count
+            SELECT fs.friend_id, f.image, f.nickname, COUNT(*) AS sticker_count
             FROM friend_sticker fs
             JOIN friend f ON fs.friend_id = f.id
             WHERE fs.sticker_id = 1 AND fs.user_id = ?
@@ -13,34 +13,34 @@ async function getRankingsByStickerId(connection, userId, stickerId) {
         `;
     } else if (stickerId === 2) {
         getRankingsQuery = `
-            SELECT fs.friend_id, f.nickname, COUNT(*) AS sticker_count
+            SELECT fs.friend_id, f.image, f.nickname, COUNT(*) AS sticker_count
             FROM friend_sticker fs
             JOIN friend f ON fs.friend_id = f.id
             WHERE fs.sticker_id = 2 AND fs.user_id = ?
             GROUP BY fs.friend_id, f.nickname
             ORDER BY sticker_count DESC
             LIMIT 3;
-        `;
+        `
     } else if (stickerId === 3) {
         getRankingsQuery = `
-            SELECT fs.friend_id, f.nickname, COUNT(*) AS sticker_count
+            SELECT fs.friend_id, f.image, f.nickname, COUNT(*) AS sticker_count
             FROM friend_sticker fs
             JOIN friend f ON fs.friend_id = f.id
             WHERE fs.sticker_id = 3 AND fs.user_id = ?
             GROUP BY fs.friend_id, f.nickname
             ORDER BY sticker_count DESC
             LIMIT 3;
-        `;
+        `
     } else if (stickerId === 4) {
         getRankingsQuery = `
-            SELECT fs.friend_id, f.nickname, COUNT(*) AS sticker_count
+            SELECT fs.friend_id, f.image, f.nickname, COUNT(*) AS sticker_count
             FROM friend_sticker fs
             JOIN friend f ON fs.friend_id = f.id
             WHERE fs.sticker_id = 4 AND fs.user_id = ?
             GROUP BY fs.friend_id, f.nickname
             ORDER BY sticker_count DESC
             LIMIT 3;
-        `;
+        `
     }
 
     const rankingRow = await connection.query(getRankingsQuery, userId);
